e4c8d30efbbca0408e8f14c0fd75099b9acdbeda
Rifqi-Ilham <rifqiilham888@gmail.com>
2026-02-10 18:08:32 +0700

    add homePage and homePageFunctionalTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,9 +41,9 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-thymeleaf-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.seleniumhq.selenium:selenium-java:${seleniumJavaVersion}")
-    testImplementation("io.github.bonigarcia:selenium-jupiter:${seleniumJupiterVersion}")
-    testImplementation("io.github.bonigarcia:webdrivermanager:${webdrivermanagerVersion}")
+    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+    testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
 }
 
 tasks.register<Test>("unitTest") {
---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/HomePageFunctionalTest.java
@@ -0,0 +1,49 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+class HomePageFunctionalTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Value("${app.baseUrl:http://localhost}")
+    private String baseUrl;
+
+    private String testUrl;
+
+    @BeforeEach
+    void setupTest() {
+        testUrl = String.format("%s:%d", baseUrl, port);
+    }
+
+    @Test
+    void pageTitle_isCorrect(ChromeDriver driver) throws Exception {
+        driver.get(testUrl);
+        String pageTitle = driver.getTitle();
+
+        assertEquals("ADV Shop", pageTitle);
+    }
+
+    @Test
+    void welcomeMessage_homePage_isCorrect(ChromeDriver driver) throws Exception {
+        driver.get(testUrl);
+        String welcomeMessage = driver.findElement(By.tagName("h3"))
+                .getText();
+
+        assertEquals("Welcome", welcomeMessage);
+    }
+}
